5b4d9167622c9600d194f550579ca18287fe359b
Chen-Jun-Ming <903639816@qq.com>
2020-12-30 02:15:08 +0800

    first

---
+++ mrzx-controller/src/main/java/com/cjm/test/TestController.java
@@ -0,0 +1,14 @@
+package com.cjm.test;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("tc")
+public class TestController {
+
+    @RequestMapping("test")
+    public String test(){
+        return "{\"a\":\"1\"}";
+    }
+}
