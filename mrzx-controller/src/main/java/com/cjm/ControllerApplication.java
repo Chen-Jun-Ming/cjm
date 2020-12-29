c28615ce077f691cec0b44c5d6a0c41db980312b
Chen-Jun-Ming <903639816@qq.com>
2020-12-30 02:19:02 +0800

    second

--- mrzx-controller/src/main/java/com/cjm/ControllerApplication.java
+++ mrzx-controller/src/main/java/com/cjm/ControllerApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ControllerApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(ControllerApplication.class,args);
     }
